25fd7f6e2ddf42b4d578182f0a26dda641a1aa0c
Marina Beder <Marina180574@feng.bu.edu.eg>
2023-04-20 12:49:40 +0200

    Add files via upload

---
+++ src/main/java/TestCompile.java
@@ -0,0 +1,22 @@
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.awt.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+public class TestCompile {
+   
+
+
+    public static void main(String[] args) throws
+            Exception{
+
+
+        
+
+}}
